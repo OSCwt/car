635ec236bcce5b514a021ebae2c8d00a213511f1
OSCW <2135311465@qq.com>
2026-04-25 19:10:53 +0800

    舵机中位ok

--- code/Servo.c
+++ code/Servo.c
@@ -1,185 +1,128 @@
 #include "main.h"
-/*
-********************************************************************************************************
-*                                               示例代码
-*                                             示例代码
-*
-*                             (c) Copyright 2021; SaiShu.Lcc.; Leo
-*                                 版权所属[北京赛曙科技有限公司]
-*
-*               本代码仅供内部学习使用，请勿商用(开源学习,请勿商用).
-*               本代码适配对应硬件电路板(代码使用CarDo智控板),
-*               具体细节请咨询专业人员(欢迎联系我们).
-*********************************************************************************************************
-*/
-
-#define YAW_CONTROL_SIGN (-1.0f)
 
 ServoStruct servoStr;
+uint32 PWMVALUE = 0;
 
-/**
-* @brief        舵机控制初始化
-* @param
-* @ref
-* @author       Leo
-* @note
-**/
+static float servo_limit_float(float value, float min_value, float max_value)
+{
+    if (value < min_value) {
+        return min_value;
+    }
+
+    if (value > max_value) {
+        return max_value;
+    }
+
+    return value;
+}
+
+static uint32 servo_angle_to_duty(float angle_deg)
+{
+    float limited_angle = servo_limit_float(angle_deg, SERVO_MOTOR_L_angle, SERVO_MOTOR_R_angle);
+    return (uint32)SERVO_MOTOR_DUTY(limited_angle);
+}
+
+static float servo_pwm_us_to_angle(signed int pwm_us)
+{
+    float limited_pwm = servo_limit_float((float)pwm_us, (float)SERVO_PWM_MIN, (float)SERVO_PWM_MAX);
+    return (limited_pwm - 500.0f) * 90.0f / 1000.0f;
+}
+
+static void servo_apply_angle(float angle_deg)
+{
+    servoStr.output_angle = servo_limit_float(angle_deg, SERVO_MOTOR_L_angle, SERVO_MOTOR_R_angle);
+    servoStr.offset_angle = servoStr.output_angle - SERVO_CENTER_ANGLE;
+    servoStr.output_duty = servo_angle_to_duty(servoStr.output_angle);
+    servoStr.thresholdMiddle = servo_angle_to_duty(SERVO_CENTER_ANGLE);
+    servoStr.thresholdLeft = servo_angle_to_duty(SERVO_MOTOR_L_angle);
+    servoStr.thresholdRight = servo_angle_to_duty(SERVO_MOTOR_R_angle);
+    servoStr.angle = servoStr.offset_angle;
+    PWMVALUE = servoStr.output_duty;
+    pwm_set_duty(SERVO_PIN, servoStr.output_duty);
+}
+
 void SERVO_Init(void)
 {
+    servoStr.thresholdLeft = servo_angle_to_duty(SERVO_MOTOR_L_angle);
+    servoStr.thresholdRight = servo_angle_to_duty(SERVO_MOTOR_R_angle);
+    servoStr.thresholdMiddle = servo_angle_to_duty(SERVO_CENTER_ANGLE);
+    servoStr.Counter = 0;
+    servoStr.Counter1 = 0;
+    servoStr.set_angle = SERVO_CENTER_ANGLE;
+    servoStr.imu_angle = 0.0f;
+    servoStr.angle_PreError = 0.0f;
+    servoStr.angel_PreDerror = 0.0f;
+    servoStr.Kp = 0.0f;
+    servoStr.Ki = 0.0f;
+    servoStr.Kd = 0.0f;
+    servoStr.angle = 0.0f;
+    servoStr.target_angle = SERVO_CENTER_ANGLE;
+    servoStr.output_angle = SERVO_CENTER_ANGLE;
+    servoStr.offset_angle = 0.0f;
+    servoStr.output_duty = servoStr.thresholdMiddle;
+    PWMVALUE = servoStr.output_duty;
 
-    pwm_init(SERVO_PIN,50,SERVO_MOTOR_DUTY(90));
-    //SERVO_SetPwmValue(servoStr.thresholdMiddle);
-
-    servoStr.thresholdLeft=SERVO_MOTOR_DUTY(SERVO_MOTOR_L_angle);
-    servoStr.thresholdRight=SERVO_MOTOR_DUTY(SERVO_MOTOR_R_angle);
-    servoStr.thresholdMiddle=SERVO_MOTOR_DUTY(90);
-    servoStr.set_angle=0;
-    servoStr.imu_angle=0;
-    servoStr.angle_PreError=0;
-    servoStr.angel_PreDerror=0;
-    servoStr.angle=0;
-    servoStr.Kp=1;
-    servoStr.Ki=0.1;
-    servoStr.Kd=0;
-
+    pwm_init(SERVO_PIN, SERVO_MOTOR_FREQ, servoStr.output_duty);
 }
+
 void servo_set(float angle)
 {
+    servoStr.target_angle = servo_limit_float(angle, SERVO_MOTOR_L_angle, SERVO_MOTOR_R_angle);
+    servo_apply_angle(servoStr.target_angle);
+}
 
-    if(angle<SERVO_MOTOR_L_angle)
-    {
-        angle=SERVO_MOTOR_L_angle;
-        servoStr.angle=-50;
-    }
-    if(angle>SERVO_MOTOR_R_angle)
-    {
-        angle=SERVO_MOTOR_R_angle;
-        servoStr.angle=50;
-    }
+void SERVO_SetAngle(float angle_deg)
+{
+    servoStr.target_angle = servo_limit_float(angle_deg, SERVO_MOTOR_L_angle, SERVO_MOTOR_R_angle);
+    servoStr.set_angle = servoStr.target_angle;
+}
 
-    pwm_set_duty(servo_pin,(uint32_t)SERVO_MOTOR_DUTY(angle));
+void SERVO_SetOffset(float offset_deg)
+{
+    float max_left_offset = SERVO_MOTOR_L_angle - SERVO_CENTER_ANGLE;
+    float max_right_offset = SERVO_MOTOR_R_angle - SERVO_CENTER_ANGLE;
+    float limited_offset = servo_limit_float(offset_deg, max_left_offset, max_right_offset);
 
+    servoStr.offset_angle = limited_offset;
+    SERVO_SetAngle(SERVO_CENTER_ANGLE + limited_offset);
+}
 
+void SERVO_Center(void)
+{
+    SERVO_SetAngle(SERVO_CENTER_ANGLE);
 }
 
 void angle_calculate(ServoStruct* pp)
 {
-    float  error,d_error,dd_error;
+    if (pp == 0) {
+        return;
+    }
 
-        error = pp->set_angle - pp->imu_angle;
-        d_error = error - pp->angle_PreError;
-        dd_error = d_error - pp->angel_PreDerror;
-
-        pp->angle_PreError = error;
-        pp->angel_PreDerror = d_error;
-
-        if( ( error < VV_DEADLINE ) && ( error > -VV_DEADLINE ) )
-        {
-            ;
-        }
-        else
-        {
-            pp->angle += (pp -> Kp * d_error + pp -> Ki * error + pp->Kd*dd_error);
-
-        }
+    pp->angle = pp->target_angle - SERVO_CENTER_ANGLE;
 }
 
 void servo_time(void)
 {
     servoStr.Counter++;
 
-    if(servoStr.Counter>10)
-    {
-        servoStr.Counter=0;
-        servoStr.imu_angle=YAW_CONTROL_SIGN * attitudeDebug.yaw_continuous_deg;
+    if (servoStr.Counter >= SERVO_UPDATE_PERIOD_MS) {
+        servoStr.Counter = 0;
         angle_calculate(&servoStr);
-        servoStr.Counter1++;
-        if(servoStr.Counter1>10)
-        {
-            servo_set(90+servoStr.angle);
-            servoStr.Counter1=0;
-        }
+        servo_apply_angle(servoStr.target_angle);
     }
-
 }
 
+void SERVO_SetPwmValue(signed int pwm)
+{
+    SERVO_SetAngle(servo_pwm_us_to_angle(pwm));
+}
 
-///**
-//* @brief        舵机输出PWM设置
-//* @param        pwm：-20000~20000
-//* @ref
-//* @author       Leo
-//* @note
-//**/
-//uint32 PWMVALUE=0;
-//void SERVO_SetPwmValue(signed int pwm)
-//{
-//   pwm = 3000 - pwm;  //左→右
-//
-//    if(pwm < SERVO_PWM_MIN)
-//        pwm = SERVO_PWM_MIN;
-//    else if(pwm > SERVO_PWM_MAX)
-//        pwm = SERVO_PWM_MAX;
-//
-//    pwm_set_duty(SERVO_PIN,pwm);
-//    PWMVALUE=pwm;
-////    TIM_SetCompare1(TIM4,pwm);
-//}
-//
-//
-///**
-//* @brief        舵机输出PWM设置（矫正后）
-//* @param        pwm：500~2500
-//* @ref
-//* @author       Leo
-//* @note
-//**/
-//uint16_t pwm_Servo = 0;
-//void SERVO_SetPwmValueCorrect(signed int pwm)
-//{
-//    pwm = 3000 - pwm;  //左→右
-//
-//    pwm -= servoStr.thresholdMiddle-SERVO_PWM_MIDDLE; //中值补偿
-//
-//    uint16_t pwmMax = 3000 - servoStr.thresholdLeft;
-//    uint16_t pwmMin = 3000 - servoStr.thresholdRight;
-//    if(pwm < pwmMin)
-//        pwm = pwmMin;
-//    else if(pwm > pwmMax)
-//        pwm = pwmMax;
-//
-//    pwm_Servo = (uint16_t)pwm;
-//    pwm_set_duty(SERVO_PIN,pwm);
-//    PWMVALUE=pwm;
-////    TIM_SetCompare1(TIM4,pwm);
-//}
-//
-///**
-//* @brief       舵机角度控制
-//* @param
-//* @ref
-//* @author
-//* @note
-//**/
-//uint16_t ServoPwm = 1500;
-//void SERVO_AngleControl(float angle)
-//{
-//    uint16_t pwm = 1500;
-//    angle = -angle;
-//    if(angle > SERVO_ANGLE_MAX)
-//        angle = SERVO_ANGLE_MAX;
-//    else if(angle < -SERVO_ANGLE_MAX)
-//        angle = -SERVO_ANGLE_MAX;
-//
-//    if(angle >= 0)  //右转
-//        pwm = (float)angle/SERVO_ANGLE_MAX * (SERVO_PWM_MAX_R-servoStr.thresholdMiddle) + servoStr.thresholdMiddle;     //绝对角度计算
-//    else if(angle < 0)      //左转
-//        pwm = (float)angle/SERVO_ANGLE_MAX * (servoStr.thresholdMiddle - SERVO_PWM_MAX_L) + servoStr.thresholdMiddle;       //绝对角度计算
-//
-//    ServoPwm = pwm;
-//    SERVO_SetPwmValue(pwm);
-//
-//}
+void SERVO_SetPwmValueCorrect(signed int pwm)
+{
+    SERVO_SetPwmValue(pwm);
+}
 
-
-
+void SERVO_AngleControl(float angle)
+{
+    SERVO_SetOffset(angle);
+}
--- code/Servo.h
+++ code/Servo.h
@@ -3,46 +3,51 @@
 
 #include "main.h"
 
-#define servo_pin ATOM1_CH1_P33_9
-#define SERVO_MOTOR_L_angle 50
-#define SERVO_MOTOR_R_angle 140
-#define SERVO_MOTOR_FREQ  50
-#define SERVO_MOTOR_DUTY(x)         ((float)PWM_DUTY_MAX/(1000.0/(float)SERVO_MOTOR_FREQ)*(0.5+(float)(x)/90.0))
+#define SERVO_PIN                       ATOM1_CH1_P33_9
+#define servo_pin                       SERVO_PIN
 
+#define SERVO_MOTOR_L_angle             50.0f
+#define SERVO_MOTOR_MID_angle           90.0f
+#define SERVO_MOTOR_R_angle             140.0f
+#define SERVO_CENTER_TRIM_DEG           1.75f
+#define SERVO_CENTER_ANGLE              (SERVO_MOTOR_MID_angle + SERVO_CENTER_TRIM_DEG)
+#define SERVO_MOTOR_FREQ                50
+#define SERVO_UPDATE_PERIOD_MS          20u
 
-#define  SERVO_PWM_MAX                  2500                        //舵机方向最大PWM：180°
-#define  SERVO_PWM_MIN                  500                         //舵机方向最小PWM：0°
-#define  SERVO_PWM_MAX_L                1150                        //舵机左向转角最大值PWM
-#define  SERVO_PWM_MAX_R                1850                        //舵机右向转角最大值PWM
-#define  SERVO_PWM_MIDDLE               1500                        //舵机中值PWM
+#define SERVO_MOTOR_DUTY(x)             ((float)PWM_DUTY_MAX / (1000.0f / (float)SERVO_MOTOR_FREQ) * (0.5f + (float)(x) / 90.0f))
 
-#define  SERVO_ANGLE_MAX                38.0f                       //舵机
+#define SERVO_PWM_MAX                   2500
+#define SERVO_PWM_MIN                   500
+#define SERVO_PWM_MAX_L                 1150
+#define SERVO_PWM_MAX_R                 1850
+#define SERVO_PWM_MIDDLE                1500
 
-#define SERVO_PIN ATOM1_CH1_P33_9
+#define SERVO_ANGLE_MAX                 38.0f
 
 extern uint32 PWMVALUE;
 
-
-
-/**
-* @brief    舵机相关
-**/
 typedef struct
 {
-    uint16_t thresholdMiddle;                   //舵机中值PWM
-    uint16_t thresholdLeft;                     //舵机左向转角最大值PWM
-    uint16_t thresholdRight;                    //舵机右向转角最大值PWM
-    uint16_t Counter;                           //线程计数器
-    uint16_t Counter1;                           //线程计数器
-    float set_angle;                              //设定值
-    float imu_angle;                              //陀螺仪数值
-    float angle_PreError;                        //角度误差,vi_Ref - vi_FeedBack
-    float angel_PreDerror;                        //角度误差之差，d_error-PreDerror;
+    uint16_t thresholdMiddle;
+    uint16_t thresholdLeft;
+    uint16_t thresholdRight;
+    uint16_t Counter;
+    uint16_t Counter1;
+
+    float set_angle;
+    float imu_angle;
+    float angle_PreError;
+    float angel_PreDerror;
     float Kp;
     float Ki;
     float Kd;
-    float angle;                                  //舵机数值,我希望这个数值是左偏或者右偏多少度
-}ServoStruct;
+    float angle;
+
+    float target_angle;
+    float output_angle;
+    float offset_angle;
+    uint32 output_duty;
+} ServoStruct;
 
 extern ServoStruct servoStr;
 
@@ -51,12 +56,12 @@ void servo_set(float angle);
 void angle_calculate(ServoStruct* pp);
 void servo_time(void);
 
+void SERVO_SetAngle(float angle_deg);
+void SERVO_SetOffset(float offset_deg);
+void SERVO_Center(void);
 
 void SERVO_SetPwmValue(signed int pwm);
 void SERVO_SetPwmValueCorrect(signed int pwm);
 void SERVO_AngleControl(float angle);
+
 #endif
-
-//===========================================  文件结束  ===========================================//
-
-
